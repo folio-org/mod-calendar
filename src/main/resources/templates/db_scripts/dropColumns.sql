d38028c782d2ea7261ee5f2696dc5f4560169f46
Roman_Barannyk <roman_barannyk@epam.com>
2020-03-31 11:22:04 +0300

    MODCAL-68 add uuid functions, drop columns for audit tables
    (cherry picked from commit c7e3ec07e2b66309bbc8a134a2f15129255aba8b)

--- src/main/resources/templates/db_scripts/dropColumns.sql
+++ src/main/resources/templates/db_scripts/dropColumns.sql
@@ -1,34 +1,19 @@
-ALTER TABLE ${myuniversity}_${mymodule}.audit_actual_opening_hours
-	DROP COLUMN IF EXISTS created_date;
-ALTER TABLE ${myuniversity}_${mymodule}.audit_actual_opening_hours
-	DROP COLUMN IF EXISTS operation;
-ALTER TABLE ${myuniversity}_${mymodule}.audit_actual_opening_hours
-	DROP COLUMN IF EXISTS orig_id;
+ALTER TABLE audit_actual_opening_hours DROP COLUMN IF EXISTS created_date;
+ALTER TABLE audit_actual_opening_hours DROP COLUMN IF EXISTS operation;
+ALTER TABLE audit_actual_opening_hours DROP COLUMN IF EXISTS orig_id;
 
-ALTER TABLE ${myuniversity}_${mymodule}.audit_openings
-	DROP COLUMN IF EXISTS created_date;
-ALTER TABLE ${myuniversity}_${mymodule}.audit_openings
-	DROP COLUMN IF EXISTS operation;
-ALTER TABLE ${myuniversity}_${mymodule}.audit_openings
-	DROP COLUMN IF EXISTS orig_id;
+ALTER TABLE audit_openings DROP COLUMN IF EXISTS created_date;
+ALTER TABLE audit_openings DROP COLUMN IF EXISTS operation;
+ALTER TABLE audit_openings DROP COLUMN IF EXISTS orig_id;
 
-ALTER TABLE ${myuniversity}_${mymodule}.audit_regular_hours
-	DROP COLUMN IF EXISTS created_date;
-ALTER TABLE ${myuniversity}_${mymodule}.audit_regular_hours
-	DROP COLUMN IF EXISTS operation;
-ALTER TABLE ${myuniversity}_${mymodule}.audit_regular_hours
-	DROP COLUMN IF EXISTS orig_id;
+ALTER TABLE audit_regular_hours	DROP COLUMN IF EXISTS created_date;
+ALTER TABLE audit_regular_hours DROP COLUMN IF EXISTS operation;
+ALTER TABLE audit_regular_hours DROP COLUMN IF EXISTS orig_id;
 
-ALTER TABLE ${myuniversity}_${mymodule}.audit_exceptions
-	DROP COLUMN IF EXISTS created_date;
-ALTER TABLE ${myuniversity}_${mymodule}.audit_exceptions
-	DROP COLUMN IF EXISTS operation;
-ALTER TABLE ${myuniversity}_${mymodule}.audit_exceptions
-	DROP COLUMN IF EXISTS orig_id;
+ALTER TABLE audit_exceptions DROP COLUMN IF EXISTS created_date;
+ALTER TABLE audit_exceptions DROP COLUMN IF EXISTS operation;
+ALTER TABLE audit_exceptions DROP COLUMN IF EXISTS orig_id;
 
-ALTER TABLE ${myuniversity}_${mymodule}.audit_exceptional_hours
-	DROP COLUMN IF EXISTS created_date;
-ALTER TABLE ${myuniversity}_${mymodule}.audit_exceptional_hours
-	DROP COLUMN IF EXISTS operation;
-ALTER TABLE ${myuniversity}_${mymodule}.audit_exceptional_hours
-	DROP COLUMN IF EXISTS orig_id;
+ALTER TABLE audit_exceptional_hours DROP COLUMN IF EXISTS created_date;
+ALTER TABLE audit_exceptional_hours DROP COLUMN IF EXISTS operation;
+ALTER TABLE audit_exceptional_hours DROP COLUMN IF EXISTS orig_id;
