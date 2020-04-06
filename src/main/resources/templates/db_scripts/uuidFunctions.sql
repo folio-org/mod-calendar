ce993d941db8c5b21541bb6dad269603dd9c83f7
Roman_Barannyk <roman_barannyk@epam.com>
2020-04-06 23:44:17 +0300

    MODCAL-68 drop deprecated functions

--- src/main/resources/templates/db_scripts/uuidFunctions.sql
+++ src/main/resources/templates/db_scripts/uuidFunctions.sql
@@ -3,3 +3,9 @@ BEGIN
 <#include "uuid.ftl">
 EXCEPTION WHEN OTHERS THEN NULL;
 END $uuidftl$;
+
+DO $$ BEGIN DROP FUNCTION count_estimate_smart; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN DROP FUNCTION count_estimate_smart_depricated; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN DROP FUNCTION set_id_injson_actual_opening_hours; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN DROP FUNCTION set_id_injson_exceptional_hours; EXCEPTION WHEN OTHERS THEN END; $$;
+DO $$ BEGIN DROP FUNCTION set_id_injson_regular_hours; EXCEPTION WHEN OTHERS THEN END; $$;
