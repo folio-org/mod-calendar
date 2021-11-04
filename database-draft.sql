11657e1ee4c6428a34ee71d190c297d43ae8f710
Noah Overcash <ncovercash@ua.edu>
2021-11-04 10:22:03 -0500

    Fix indentation of database and docs to FOLIO 2-space standard

--- database-draft.sql
+++ database-draft.sql
@@ -1,19 +1,19 @@
 CREATE TYPE "weekday" AS (
-    'sunday',
-    'monday',
-    'tuesday',
-    'wednesday',
-    'thursday',
-    'friday',
-    'saturday'
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday'
 );
 
 CREATE TABLE "calendar_v2"."calendars" (
-    "id" uuid NOT NULL,
-    "name" text NOT NULL,
-    "start_date" date NOT NULL,
-    "end_date" date NOT NULL,
-    CONSTRAINT "calendar_id" PRIMARY KEY ("id")
+  "id" uuid NOT NULL,
+  "name" text NOT NULL,
+  "start_date" date NOT NULL,
+  "end_date" date NOT NULL,
+  CONSTRAINT "calendar_id" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
 CREATE INDEX "calendar_start_end_date" ON "calendar_v2"."calendars" USING btree ("start_date", "end_date");
@@ -23,13 +23,13 @@ COMMENT ON COLUMN "calendar_v2"."calendars"."start_date" IS 'no TZ';
 COMMENT ON COLUMN "calendar_v2"."calendars"."end_date" IS 'no TZ';
 
 CREATE TABLE "calendar_v2"."exception_hours" (
-    "id" uuid NOT NULL,
-    "exception_id" uuid NOT NULL,
-    "start_date" date NOT NULL,
-    "end_date" date NOT NULL,
-    "open_start_time" time without time zone,
-    "open_end_time" time without time zone,
-    CONSTRAINT "exception_hours_id" PRIMARY KEY ("id")
+  "id" uuid NOT NULL,
+  "exception_id" uuid NOT NULL,
+  "start_date" date NOT NULL,
+  "end_date" date NOT NULL,
+  "open_start_time" time without time zone,
+  "open_end_time" time without time zone,
+  CONSTRAINT "exception_hours_id" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
 CREATE INDEX "exception_hours_exception_id" ON "calendar_v2"."exception_hours" USING btree ("exception_id");
@@ -42,11 +42,11 @@ COMMENT ON COLUMN "calendar_v2"."exception_hours"."open_end_time" IS 'No TZ';
 
 
 CREATE TABLE "calendar_v2"."exceptions" (
-    "id" uuid NOT NULL,
-    "calendar_id" uuid NOT NULL,
-    "start_date" date NOT NULL,
-    "end_date" date NOT NULL,
-    CONSTRAINT "exceptions_id" PRIMARY KEY ("id")
+  "id" uuid NOT NULL,
+  "calendar_id" uuid NOT NULL,
+  "start_date" date NOT NULL,
+  "end_date" date NOT NULL,
+  CONSTRAINT "exceptions_id" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
 CREATE INDEX "exceptions_calendar_id" ON "calendar_v2"."exceptions" USING btree ("calendar_id");
@@ -55,13 +55,13 @@ CREATE INDEX "exceptions_start_date_end_date" ON "calendar_v2"."exceptions" USIN
 COMMENT ON TABLE "calendar_v2"."exceptions" IS 'Metadata for each exception';
 
 CREATE TABLE "calendar_v2"."normal_hours" (
-    "id" uuid NOT NULL,
-    "calendar_id" uuid NOT NULL,
-    "start_day" weekday NOT NULL,
-    "start_time" time without time zone NOT NULL,
-    "end_day" weekday NOT NULL,
-    "end_time" time without time zone NOT NULL,
-    CONSTRAINT "normal_hours_id" PRIMARY KEY ("id")
+  "id" uuid NOT NULL,
+  "calendar_id" uuid NOT NULL,
+  "start_day" weekday NOT NULL,
+  "start_time" time without time zone NOT NULL,
+  "end_day" weekday NOT NULL,
+  "end_time" time without time zone NOT NULL,
+  CONSTRAINT "normal_hours_id" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
 CREATE INDEX "normal_hours_calendar_id" ON "calendar_v2"."normal_hours" USING btree ("calendar_id");
@@ -71,8 +71,8 @@ COMMENT ON COLUMN "calendar_v2"."normal_hours"."start_day" IS 'no TZ';
 COMMENT ON COLUMN "calendar_v2"."normal_hours"."end_day" IS 'no TZ';
 
 CREATE TABLE "calendar_v2"."service_point_calendars" (
-    "service_point_id" uuid NOT NULL,
-    "calendar_id" uuid NOT NULL
+  "service_point_id" uuid NOT NULL,
+  "calendar_id" uuid NOT NULL
 ) WITH (oids = false);
 
 CREATE INDEX "service_point_calendars_calendar_id" ON "calendar_v2"."service_point_calendars" USING btree ("calendar_id");
