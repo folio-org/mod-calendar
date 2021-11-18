f8cbdc1ef9d02a0b31e23542e275e3d864646e7e
Noah Overcash <ncovercash@ua.edu>
2021-11-18 10:37:50 -0600

    Explicitly state Java soure compatibility version

--- src/main/java/org/folio/calendar/controller/HelloWorldController.java
+++ src/main/java/org/folio/calendar/controller/HelloWorldController.java
@@ -1,10 +1,8 @@
 package org.folio.calendar.controller;
 
-import javax.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import org.folio.calendar.domain.dto.Example;
 import org.folio.calendar.rest.resource.HelloApi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +17,6 @@ public class HelloWorldController implements HelloApi {
   public ResponseEntity<Example> getHelloWorld() {
     Example sampleResponse = new Example();
     sampleResponse.setHello("heya!");
-    return new ResponseEntity<Example>(sampleResponse, HttpStatus.OK);
+    return new ResponseEntity<>(sampleResponse, HttpStatus.OK);
   }
 }
