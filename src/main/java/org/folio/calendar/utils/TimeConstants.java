7d98ad3850c794f948ee278d1fee2c13e64afa94
Noah Overcash <me@ncovercash.dev>
2021-12-14 13:12:13 -0600

    Add OpeningHourRange ALL_DAY constant

--- src/main/java/org/folio/calendar/utils/TimeConstants.java
+++ src/main/java/org/folio/calendar/utils/TimeConstants.java
@@ -2,6 +2,7 @@ package org.folio.calendar.utils;
 
 import java.time.LocalTime;
 import lombok.experimental.UtilityClass;
+import org.folio.calendar.domain.dto.OpeningHourRange;
 
 @UtilityClass
 public class TimeConstants {
@@ -14,4 +15,9 @@ public class TimeConstants {
    * The last minute in the day
    */
   public static final LocalTime DAY_MAX = LocalTime.of(23, 59);
+
+  /**
+   * An OpeningHourRange that lasts all day
+   */
+  public static final OpeningHourRange ALL_DAY = new OpeningHourRange(DAY_MIN, DAY_MAX);
 }
