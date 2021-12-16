9efb45d77205ce061132974effa80980210f0a12
Noah Overcash <me@ncovercash.dev>
2021-12-15 23:55:12 -0500

    Add calendar overlap unit tests

--- src/test/java/org/folio/calendar/testconstants/Calendars.java
+++ src/test/java/org/folio/calendar/testconstants/Calendars.java
@@ -14,34 +14,59 @@ public class Calendars {
     .startDate(Dates.DATE_2021_01_01)
     .endDate(Dates.DATE_2021_01_01)
     .build();
-  public static final Calendar CALENDAR_2021_01_01_TO_2021_04_31 = Calendar
+  public static final Calendar CALENDAR_2021_01_01_TO_2021_04_30 = Calendar
     .builder()
     .startDate(Dates.DATE_2021_01_01)
-    .endDate(Dates.DATE_2021_04_31)
+    .endDate(Dates.DATE_2021_04_30)
     .build();
   public static final Calendar CALENDAR_2021_01_01_TO_2021_05_01 = Calendar
     .builder()
     .startDate(Dates.DATE_2021_01_01)
     .endDate(Dates.DATE_2021_05_01)
     .build();
+  public static final Calendar CALENDAR_2021_01_01_TO_2021_07_04 = Calendar
+    .builder()
+    .startDate(Dates.DATE_2021_01_01)
+    .endDate(Dates.DATE_2021_07_04)
+    .build();
+  public static final Calendar CALENDAR_2021_01_02_TO_2021_01_02 = Calendar
+    .builder()
+    .startDate(Dates.DATE_2021_01_02)
+    .endDate(Dates.DATE_2021_01_02)
+    .build();
+  public static final Calendar CALENDAR_2021_03_16_TO_2021_03_16 = Calendar
+    .builder()
+    .startDate(Dates.DATE_2021_03_16)
+    .endDate(Dates.DATE_2021_03_16)
+    .build();
+  public static final Calendar CALENDAR_2021_03_16_TO_2021_04_30 = Calendar
+    .builder()
+    .startDate(Dates.DATE_2021_03_16)
+    .endDate(Dates.DATE_2021_04_30)
+    .build();
   public static final Calendar CALENDAR_2021_03_16_TO_2021_05_01 = Calendar
     .builder()
     .startDate(Dates.DATE_2021_03_16)
     .endDate(Dates.DATE_2021_05_01)
     .build();
-  public static final Calendar CALENDAR_2021_03_16_TO_2021_04_31 = Calendar
+  public static final Calendar CALENDAR_2021_04_30_TO_2021_04_30 = Calendar
     .builder()
-    .startDate(Dates.DATE_2021_03_16)
-    .endDate(Dates.DATE_2021_04_31)
+    .startDate(Dates.DATE_2021_04_30)
+    .endDate(Dates.DATE_2021_04_30)
+    .build();
+  public static final Calendar CALENDAR_2021_04_30_TO_2021_09_22 = Calendar
+    .builder()
+    .startDate(Dates.DATE_2021_04_30)
+    .endDate(Dates.DATE_2021_09_22)
     .build();
   public static final Calendar CALENDAR_2021_05_01_TO_2021_09_22 = Calendar
     .builder()
     .startDate(Dates.DATE_2021_05_01)
     .endDate(Dates.DATE_2021_09_22)
     .build();
-  public static final Calendar CALENDAR_2021_04_31_TO_2021_09_22 = Calendar
+  public static final Calendar CALENDAR_2021_07_04_TO_2021_09_22 = Calendar
     .builder()
-    .startDate(Dates.DATE_2021_04_31)
+    .startDate(Dates.DATE_2021_07_04)
     .endDate(Dates.DATE_2021_09_22)
     .build();
 }
