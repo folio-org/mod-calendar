09c5678fd314a681fef133eaf818ca352ede37b5
Noah Overcash <me@ncovercash.dev>
2021-12-15 21:47:17 -0500

    Add TimeConstants tests

--- src/test/java/org/folio/calendar/utils/DateUtilsTest.java
+++ src/test/java/org/folio/calendar/utils/DateUtilsTest.java
@@ -3,7 +3,7 @@ package org.folio.calendar.utils;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-import org.folio.calendar.testutils.TimeConstants;
+import org.folio.calendar.testutils.TestTimeConstants;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -13,56 +13,56 @@ public class DateUtilsTest {
   @Test
   void testHourMinutesFormatting() {
     assertThat(
-      DateUtils.toTimeString(TimeConstants.TIME_00_00),
-      is(equalTo(TimeConstants.TIME_00_00_STRING_HH_mm))
+      DateUtils.toTimeString(TestTimeConstants.TIME_00_00),
+      is(equalTo(TestTimeConstants.TIME_00_00_STRING_HH_mm))
     );
     assertThat(
-      DateUtils.toTimeString(TimeConstants.TIME_04_00),
-      is(equalTo(TimeConstants.TIME_04_00_STRING_HH_mm))
+      DateUtils.toTimeString(TestTimeConstants.TIME_04_00),
+      is(equalTo(TestTimeConstants.TIME_04_00_STRING_HH_mm))
     );
     assertThat(
-      DateUtils.toTimeString(TimeConstants.TIME_12_30),
-      is(equalTo(TimeConstants.TIME_12_30_STRING_HH_mm))
+      DateUtils.toTimeString(TestTimeConstants.TIME_12_30),
+      is(equalTo(TestTimeConstants.TIME_12_30_STRING_HH_mm))
     );
     assertThat(
-      DateUtils.toTimeString(TimeConstants.TIME_14_00),
-      is(equalTo(TimeConstants.TIME_14_00_STRING_HH_mm))
+      DateUtils.toTimeString(TestTimeConstants.TIME_14_00),
+      is(equalTo(TestTimeConstants.TIME_14_00_STRING_HH_mm))
     );
     assertThat(
-      DateUtils.toTimeString(TimeConstants.TIME_23_00),
-      is(equalTo(TimeConstants.TIME_23_00_STRING_HH_mm))
+      DateUtils.toTimeString(TestTimeConstants.TIME_23_00),
+      is(equalTo(TestTimeConstants.TIME_23_00_STRING_HH_mm))
     );
     assertThat(
-      DateUtils.toTimeString(TimeConstants.TIME_23_59),
-      is(equalTo(TimeConstants.TIME_23_59_STRING_HH_mm))
+      DateUtils.toTimeString(TestTimeConstants.TIME_23_59),
+      is(equalTo(TestTimeConstants.TIME_23_59_STRING_HH_mm))
     );
   }
 
   @Test
   void testHourMinutesParsing() {
     assertThat(
-      DateUtils.fromTimeString(TimeConstants.TIME_00_00_STRING_HH_mm),
-      is(equalTo(TimeConstants.TIME_00_00))
+      DateUtils.fromTimeString(TestTimeConstants.TIME_00_00_STRING_HH_mm),
+      is(equalTo(TestTimeConstants.TIME_00_00))
     );
     assertThat(
-      DateUtils.fromTimeString(TimeConstants.TIME_04_00_STRING_HH_mm),
-      is(equalTo(TimeConstants.TIME_04_00))
+      DateUtils.fromTimeString(TestTimeConstants.TIME_04_00_STRING_HH_mm),
+      is(equalTo(TestTimeConstants.TIME_04_00))
     );
     assertThat(
-      DateUtils.fromTimeString(TimeConstants.TIME_12_30_STRING_HH_mm),
-      is(equalTo(TimeConstants.TIME_12_30))
+      DateUtils.fromTimeString(TestTimeConstants.TIME_12_30_STRING_HH_mm),
+      is(equalTo(TestTimeConstants.TIME_12_30))
     );
     assertThat(
-      DateUtils.fromTimeString(TimeConstants.TIME_14_00_STRING_HH_mm),
-      is(equalTo(TimeConstants.TIME_14_00))
+      DateUtils.fromTimeString(TestTimeConstants.TIME_14_00_STRING_HH_mm),
+      is(equalTo(TestTimeConstants.TIME_14_00))
     );
     assertThat(
-      DateUtils.fromTimeString(TimeConstants.TIME_23_00_STRING_HH_mm),
-      is(equalTo(TimeConstants.TIME_23_00))
+      DateUtils.fromTimeString(TestTimeConstants.TIME_23_00_STRING_HH_mm),
+      is(equalTo(TestTimeConstants.TIME_23_00))
     );
     assertThat(
-      DateUtils.fromTimeString(TimeConstants.TIME_23_59_STRING_HH_mm),
-      is(equalTo(TimeConstants.TIME_23_59))
+      DateUtils.fromTimeString(TestTimeConstants.TIME_23_59_STRING_HH_mm),
+      is(equalTo(TestTimeConstants.TIME_23_59))
     );
   }
 }
---
+++ src/test/java/org/folio/calendar/utils/TimeConstantsTest.java
@@ -0,0 +1,62 @@
+package org.folio.calendar.utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import org.folio.calendar.testutils.TestTimeConstants;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+public class TimeConstantsTest {
+
+  @Test
+  void testMinTimeIsMidnight() {
+    assertThat(TimeConstants.TIME_MIN, is(equalTo(LocalTime.MIN)));
+    assertThat(TimeConstants.TIME_MIN, is(equalTo(TestTimeConstants.TIME_00_00)));
+    assertThat(
+      TimeConstants.TIME_MIN_STRING,
+      is(equalTo(TestTimeConstants.TIME_00_00_STRING_HH_mm))
+    );
+    assertThat(
+      DateUtils.toTimeString(TimeConstants.TIME_MIN),
+      is(equalTo(TestTimeConstants.TIME_00_00_STRING_HH_mm))
+    );
+    assertThat(
+      DateUtils.fromTimeString(TimeConstants.TIME_MIN_STRING),
+      is(equalTo(TestTimeConstants.TIME_00_00))
+    );
+  }
+
+  @Test
+  void testMaxTimeIs23Hours59Minutes() {
+    assertThat(TimeConstants.TIME_MAX, is(equalTo(LocalTime.MAX.truncatedTo(ChronoUnit.MINUTES))));
+    assertThat(TimeConstants.TIME_MAX, is(equalTo(TestTimeConstants.TIME_23_59)));
+    assertThat(
+      TimeConstants.TIME_MAX_STRING,
+      is(equalTo(TestTimeConstants.TIME_23_59_STRING_HH_mm))
+    );
+    assertThat(
+      DateUtils.toTimeString(TimeConstants.TIME_MAX),
+      is(equalTo(TestTimeConstants.TIME_23_59_STRING_HH_mm))
+    );
+    assertThat(
+      DateUtils.fromTimeString(TimeConstants.TIME_MAX_STRING),
+      is(equalTo(TestTimeConstants.TIME_23_59))
+    );
+  }
+
+  @Test
+  void testAllDayRange() {
+    assertThat(
+      TimeConstants.ALL_DAY.getStartTime(),
+      is(equalTo(TestTimeConstants.TIME_00_00_STRING_HH_mm))
+    );
+    assertThat(
+      TimeConstants.ALL_DAY.getEndTime(),
+      is(equalTo(TestTimeConstants.TIME_23_59_STRING_HH_mm))
+    );
+  }
+}
