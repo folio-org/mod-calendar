622c195a5ebc960368f2ac396d6dde64ce854d4c
Noah Overcash <me@ncovercash.dev>
2021-12-23 16:48:02 -0500

    Remove additional import in NormalOpening test

--- src/test/java/org/folio/calendar/unit/domain/entity/NormalOpeningTruncationTest.java
+++ src/test/java/org/folio/calendar/unit/domain/entity/NormalOpeningTruncationTest.java
@@ -5,7 +5,6 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 import org.folio.calendar.domain.entity.NormalOpening;
-import org.folio.calendar.testconstants.NormalOpenings;
 import org.folio.calendar.testconstants.Times;
 import org.junit.jupiter.api.Test;
 
