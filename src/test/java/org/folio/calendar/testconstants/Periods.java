6ca6de38ef989f90598e030c3b663e6092bec1c5
Noah Overcash <me@ncovercash.dev>
2021-12-24 06:24:50 -0500

    Add new constants to support additional integration tests (#17)

--- src/test/java/org/folio/calendar/testconstants/Periods.java
+++ src/test/java/org/folio/calendar/testconstants/Periods.java
@@ -141,6 +141,33 @@ public class Periods {
       )
     );
 
+  public static final Period PERIOD_FULL_EXAMPLE_F = PERIOD_2021_01_01_TO_2021_04_30
+    .withId(UUIDs.UUID_A)
+    .withServicePointId(UUIDs.UUID_0)
+    .withName(Names.NAME_1)
+    .withOpeningDays(
+      Arrays.asList(
+        OpeningDayRelativeConstants.SUNDAY_OPEN_ALL_DAY,
+        OpeningDayRelativeConstants.MONDAY_OPEN_04_00_TO_14_59,
+        OpeningDayRelativeConstants.TUESDAY_OPEN_ALL_DAY,
+        OpeningDayRelativeConstants.WEDNESDAY_OPEN_23_00_TO_23_59,
+        OpeningDayRelativeConstants.THURSDAY_OPEN_ALL_DAY,
+        OpeningDayRelativeConstants.FRIDAY_OPEN_ALL_DAY,
+        OpeningDayRelativeConstants.SATURDAY_OPEN_ALL_DAY
+      )
+    );
+
+  public static final Period PERIOD_FULL_EXAMPLE_G = PERIOD_2021_05_01_TO_2021_09_22
+    .withId(UUIDs.UUID_B)
+    .withServicePointId(UUIDs.UUID_0)
+    .withName(Names.NAME_2)
+    .withOpeningDays(
+      Arrays.asList(
+        OpeningDayRelativeConstants.MONDAY_OPEN_00_00_TO_12_30_AND_23_00_TO_23_59,
+        OpeningDayRelativeConstants.THURSDAY_OPEN_ALL_DAY
+      )
+    );
+
   public static final Period PERIOD_FULL_EXCEPTIONAL_A = PERIOD_2021_01_01_TO_2021_12_31
     .withId(UUIDs.UUID_A)
     .withServicePointId(UUIDs.UUID_0)
@@ -164,4 +191,22 @@ public class Periods {
     .withServicePointId(UUIDs.UUID_9)
     .withName(Names.NAME_4)
     .withOpeningDays(Arrays.asList(OpeningDayRelativeConstants.EXCEPTIONAL_OPEN_00_00_TO_14_59));
+
+  public static final Period PERIOD_FULL_EXCEPTIONAL_E = PERIOD_2021_01_01_TO_2021_01_04
+    .withId(UUIDs.UUID_E)
+    .withServicePointId(UUIDs.UUID_0)
+    .withName(Names.NAME_1)
+    .withOpeningDays(Arrays.asList(OpeningDayRelativeConstants.EXCEPTIONAL_CLOSED));
+
+  public static final Period PERIOD_FULL_EXCEPTIONAL_F = PERIOD_2021_03_16_TO_2021_04_30
+    .withId(UUIDs.UUID_F)
+    .withServicePointId(UUIDs.UUID_0)
+    .withName(Names.NAME_2)
+    .withOpeningDays(Arrays.asList(OpeningDayRelativeConstants.EXCEPTIONAL_OPEN_ALL_DAY));
+
+  public static final Period PERIOD_FULL_EXCEPTIONAL_G = PERIOD_2021_07_04_TO_2021_09_22
+    .withId(UUIDs.UUID_0)
+    .withServicePointId(UUIDs.UUID_0)
+    .withName(Names.NAME_3)
+    .withOpeningDays(Arrays.asList(OpeningDayRelativeConstants.EXCEPTIONAL_OPEN_04_00_TO_14_59));
 }
