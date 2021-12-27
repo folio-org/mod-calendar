876b5446c5a125517c05f5058a8c0bc9f3fe5e14
Noah Overcash <me@ncovercash.dev>
2021-12-27 08:09:18 -0500

    Remove duplicate import

--- src/main/java/org/folio/calendar/domain/entity/NormalOpening.java
+++ src/main/java/org/folio/calendar/domain/entity/NormalOpening.java
@@ -6,7 +6,6 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
