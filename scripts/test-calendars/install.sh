afd05fd8319b82b8fba18cbe4679bdcea7c1b133
Noah Overcash <me@ncovercash.dev>
2022-01-01 01:43:18 -0500

    Add protocol to install script

--- scripts/test-calendars/install.sh
+++ scripts/test-calendars/install.sh
@@ -2,22 +2,22 @@
 
 # You may manually specify an OKAPI_HOST and OKAPI_TOKEN through environment variables, if not testing locally.
 # For example:
-#   OKAPI_HOST=folio-testing-okapi.dev.folio.org \
+#   OKAPI_HOST=https://folio-testing-okapi.dev.folio.org \
 #   OKAPI_TOKEN=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkaWt1X2FkbWluIiwidXNlcl9pZCI6Ijk1NTM2ODc1LWNmNGYtNWZjMC04NjNjLTVkYTAwYWIzMzBkNCIsImlhdCI6MTY0MDkyMjEyMywidGVuYW50IjoiZGlrdSJ9.8AAqx6kHVL777wJTGBUyUGLGShLfX6QMnE_fX0kY5h0 \
 #   ./install.sh
 
 if [ -z "$OKAPI_HOST" ]; then
-  OKAPI_HOST="localhost:8080"
+  OKAPI_HOST="http://localhost:8080"
 fi
 
 for file in *.json; do
   echo "Adding calendar: $file"
-  curl --fail --silent --show-error -o /dev/null -X POST \
+  curl -L --fail --silent --show-error -o /dev/null -X POST \
     -H "X-Okapi-Tenant: diku" \
     -H "X-Okapi-Token: $OKAPI_TOKEN" \
     -H "Content-type: application/json" \
     -d @$file \
-    http://$OKAPI_HOST/calendar/periods/3a40852d-49fd-4df2-a1f9-6e2641a6e91f/period
+    $OKAPI_HOST/calendar/periods/3a40852d-49fd-4df2-a1f9-6e2641a6e91f/period
 done
 
 echo "Finished"
