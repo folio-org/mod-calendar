dd6c530868b7928f35f4e9707856028e17fac3ca
Noah Overcash <ncovercash@ua.edu>
2022-01-20 20:43:44 -0600

    Add main migration code (#33)

---
+++ src/main/java/org/folio/calendar/domain/legacymapper/RMBHoursMapper.java
@@ -0,0 +1,47 @@
+package org.folio.calendar.domain.legacymapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+import org.folio.calendar.domain.dto.OpeningDayRelative;
+import org.springframework.jdbc.core.RowMapper;
+
+/**
+ * Conversion class for data from the regular_hours table to List<OpeningDayRelative>
+ */
+@Data
+@Log4j2
+public class RMBHoursMapper implements RowMapper<List<OpeningDayRelative>> {
+
+  public static final String GET_RMB_OPENING_HOURS =
+    "SELECT jsonb FROM regular_hours WHERE jsonb->'openingId' = ?";
+
+  protected final ObjectMapper mapper;
+
+  public List<OpeningDayRelative> mapRow(ResultSet result, int rowNum) {
+    try {
+      return Arrays.asList(
+        mapper.treeToValue(
+          mapper.readTree(result.getString("jsonb")).get("openingDays"),
+          OpeningDayRelative[].class
+        )
+      );
+    } catch (JsonProcessingException e) {
+      log.error("Could not parse regular_hours JSON");
+      log.error(e);
+
+      return new ArrayList<>();
+    } catch (SQLException e) {
+      log.error("SQLException on parsing regular_hours");
+      log.error(e);
+
+      return new ArrayList<>();
+    }
+  }
+}
---
+++ src/main/java/org/folio/calendar/service/CustomTenantService.java
@@ -0,0 +1,127 @@
+package org.folio.calendar.service;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Priority;
+import liquibase.exception.LiquibaseException;
+import lombok.extern.log4j.Log4j2;
+import org.folio.calendar.controller.CalendarController;
+import org.folio.calendar.domain.dto.Period;
+import org.folio.calendar.domain.legacymapper.RMBOpeningMapper;
+import org.folio.calendar.exception.AbstractCalendarException;
+import org.folio.spring.FolioExecutionContext;
+import org.folio.spring.exception.TenantUpgradeException;
+import org.folio.spring.liquibase.FolioSpringLiquibase;
+import org.folio.spring.service.TenantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+@Priority(-10)
+public class CustomTenantService extends TenantService {
+
+  public static final String IS_RMB_SQL =
+    "SELECT EXISTS(SELECT 1 FROM pg_tables WHERE schemaname=? AND tablename=?)";
+  public static final String RMB_INTERNAL = "rmb_internal";
+  public static final String GET_RMB_OPENINGS = "SELECT jsonb FROM openings";
+
+  // must be redeclared to change visibility
+  /** the JDBC template to use for database queries */
+  protected final JdbcTemplate jdbcTemplate;
+  /** information about the environment FOLIO is running in */
+  protected final FolioExecutionContext context;
+  /** a custom instance of {@link liquibase.integration.spring.SpringLiquibase SpringLiquibase} */
+  protected final FolioSpringLiquibase folioSpringLiquibase;
+
+  protected final CalendarService calendarService;
+
+  /**
+   * Constructor for a new CustomTenantService.  Directly wraps the original {@link TenantService TenantService} constructor.
+   * @param jdbcTemplate the JDBC template to use for database queries
+   * @param context information about the environment FOLIO is running in
+   * @param folioSpringLiquibase a custom instance of {@link liquibase.integration.spring.SpringLiquibase SpringLiquibase}
+   * @param calendarService an autowired CalendarService for adding calendars when migrating
+   */
+  @Autowired
+  public CustomTenantService(
+    JdbcTemplate jdbcTemplate,
+    FolioExecutionContext context,
+    FolioSpringLiquibase folioSpringLiquibase,
+    CalendarService calendarService
+  ) {
+    super(jdbcTemplate, context, folioSpringLiquibase);
+    // initialized here in addition to super as super's fields are private
+    this.jdbcTemplate = jdbcTemplate;
+    this.context = context;
+    this.folioSpringLiquibase = folioSpringLiquibase;
+    this.calendarService = calendarService;
+  }
+
+  /**
+   * Get the name of the schema to create.  Re-defined from {@link TenantService#getSchemaName} due to visibility
+   * @return a name for the module's schema
+   */
+  protected String getDBSchemaName() {
+    return context.getFolioModuleMetadata().getDBSchemaName(context.getTenantId());
+  }
+
+  /**
+   * Perform all applicable transformations for a new tenant environment
+   */
+  @Override
+  public void createTenant() {
+    boolean shouldMigrate = jdbcTemplate.query(
+      IS_RMB_SQL,
+      (ResultSet resultSet) -> resultSet.next() && resultSet.getBoolean(1),
+      this.getDBSchemaName(),
+      RMB_INTERNAL
+    );
+
+    List<Period> periodsToMigrate = new ArrayList<>();
+
+    if (shouldMigrate) {
+      log.info("Existing RMB installation detected.  Attempting migration.");
+
+      ObjectMapper mapper = new ObjectMapper()
+        .registerModule(new JavaTimeModule())
+        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        .setSerializationInclusion(Include.NON_NULL);
+
+      periodsToMigrate =
+        jdbcTemplate.query(GET_RMB_OPENINGS, new RMBOpeningMapper(jdbcTemplate, mapper));
+
+      log.info(String.format("Found %d periods to migrate", periodsToMigrate.size()));
+      log.debug(periodsToMigrate);
+    }
+
+    // from original folio-spring-base
+    this.folioSpringLiquibase.setDefaultSchema(this.getDBSchemaName());
+    log.info("About to start liquibase update for tenant [{}]", context.getTenantId());
+
+    try {
+      this.folioSpringLiquibase.performLiquibaseUpdate();
+    } catch (LiquibaseException e) {
+      throw new TenantUpgradeException(e);
+    }
+
+    log.info("Liquibase update for tenant [{}] executed successfully", context.getTenantId());
+    // end original folio-spring-base
+
+    for (Period period : periodsToMigrate) {
+      try {
+        log.info(String.format("Attempting to save calendar with ID %s", period.getId()));
+        this.calendarService.createCalendarFromPeriod(period, period.getServicePointId());
+      } catch (AbstractCalendarException e) {
+        log.error(String.format("Could not save calendar with ID %s", period.getId()));
+        log.error(e);
+      }
+    }
+  }
+}
