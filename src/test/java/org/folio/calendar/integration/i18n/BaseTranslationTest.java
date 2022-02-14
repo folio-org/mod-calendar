d1fd87d5c657988ed8dad3debba56f855a51ee9c
Noah Overcash <ncovercash@ua.edu>
2022-02-14 13:23:49 -0600

    Finalize translation coverage

--- src/test/java/org/folio/calendar/integration/i18n/BaseTranslationTest.java
+++ src/test/java/org/folio/calendar/integration/i18n/BaseTranslationTest.java
@@ -1,9 +1,11 @@
 package org.folio.calendar.integration.i18n;
 
+import java.util.Locale;
 import org.folio.calendar.i18n.TranslationConfiguration;
 import org.folio.calendar.i18n.TranslationService;
 import org.folio.calendar.integration.BaseApiTest;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
@@ -18,8 +20,10 @@ abstract class BaseTranslationTest extends BaseApiTest {
   @Autowired
   ResourcePatternResolver resourceResolver;
 
+  @BeforeEach
   @AfterEach
   void reset() {
+    Locale.setDefault(Locale.US);
     translationService.clearCache();
     translationConfiguration.reset();
   }
