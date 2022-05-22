d55ea614b5ea2ea59bcaacea30a22ba27eae5909
Noah Overcash <ncovercash@ua.edu>
2022-05-22 14:46:09 -0500

    Add skeleton OpeningHoursController

---
+++ src/main/java/org/folio/calendar/controller/OpeningHoursController.java
@@ -0,0 +1,91 @@
+package org.folio.calendar.controller;
+
+import io.swagger.annotations.ApiParam;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import lombok.extern.log4j.Log4j2;
+import org.folio.calendar.domain.dto.AdjacentOpeningsDTO;
+import org.folio.calendar.domain.dto.CalendarCollectionDTO;
+import org.folio.calendar.domain.dto.CalendarDTO;
+import org.folio.calendar.domain.dto.SingleDayOpeningCollectionDTO;
+import org.folio.calendar.rest.resource.OpeningHoursApi;
+import org.folio.calendar.service.CalendarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Main Calendar API controller
+ */
+@Log4j2
+@RestController
+@RequestMapping(value = "/")
+public final class OpeningHoursController implements OpeningHoursApi {
+
+  @Autowired
+  public CalendarService calendarService;
+
+  /** {@inheritDoc} */
+  @Override
+  public ResponseEntity<CalendarDTO> createCalendar(CalendarDTO calendar) {
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ResponseEntity<Void> deleteCalendars(List<UUID> calendars) {
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ResponseEntity<AdjacentOpeningsDTO> getAdjacentOpenings(
+    UUID servicePointId,
+    LocalDate date
+  ) {
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ResponseEntity<CalendarCollectionDTO> getAllCalendars(
+    List<UUID> servicePointId,
+    LocalDate startDate,
+    LocalDate endDate,
+    Integer offset,
+    Integer limit
+  ) {
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ResponseEntity<SingleDayOpeningCollectionDTO> getAllOpenings(
+    @ApiParam(value = "The service point to calculate openings on", required = true) @PathVariable(
+      "servicePointId"
+    ) UUID servicePointId,
+    LocalDate startDate,
+    LocalDate endDate,
+    Boolean includeClosed,
+    Integer offset,
+    Integer limit
+  ) {
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ResponseEntity<CalendarCollectionDTO> getCalendars(List<UUID> calendars) {
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ResponseEntity<CalendarDTO> updateCalendar(UUID calendarId, CalendarDTO calendar) {
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
+}
