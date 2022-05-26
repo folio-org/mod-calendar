03d0b2c49042d891fddab2e96d486017379b46e7
Noah Overcash <ncovercash@ua.edu>
2022-05-25 23:01:17 -0500

    Add LocalTimeFromRange tests

---
+++ src/test/java/org/folio/calendar/unit/utils/LocalTimeFromRangeTest.java
@@ -0,0 +1,47 @@
+package org.folio.calendar.unit.utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+
+import org.folio.calendar.testconstants.Times;
+import org.folio.calendar.utils.LocalTimeFromRange;
+import org.junit.jupiter.api.Test;
+
+class LocalTimeFromRangeTest {
+
+  static final LocalTimeFromRange START_04_00 = new LocalTimeFromRange(
+    Times.TIME_04_00,
+    null,
+    true
+  );
+  static final LocalTimeFromRange START_15_00 = new LocalTimeFromRange(
+    Times.TIME_15_00,
+    null,
+    true
+  );
+  static final LocalTimeFromRange END_04_00 = new LocalTimeFromRange(Times.TIME_04_00, null, false);
+  static final LocalTimeFromRange END_15_00 = new LocalTimeFromRange(Times.TIME_15_00, null, false);
+
+  @Test
+  void testCompare() {
+    // we do not care about self -> self as that order is indeterminate and
+    // stable sort is not needed for the use case
+    assertThat(START_04_00.compareTo(START_15_00), is(lessThan(0)));
+    assertThat(START_04_00.compareTo(END_04_00), is(lessThan(0)));
+    assertThat(START_04_00.compareTo(END_15_00), is(lessThan(0)));
+
+    assertThat(START_15_00.compareTo(START_04_00), is(greaterThan(0)));
+    assertThat(START_15_00.compareTo(END_04_00), is(greaterThan(0)));
+    assertThat(START_15_00.compareTo(END_15_00), is(lessThan(0)));
+
+    assertThat(END_04_00.compareTo(START_04_00), is(greaterThan(0)));
+    assertThat(END_04_00.compareTo(START_15_00), is(lessThan(0)));
+    assertThat(END_04_00.compareTo(END_15_00), is(lessThan(0)));
+
+    assertThat(END_15_00.compareTo(START_04_00), is(greaterThan(0)));
+    assertThat(END_15_00.compareTo(START_15_00), is(greaterThan(0)));
+    assertThat(END_15_00.compareTo(END_04_00), is(greaterThan(0)));
+  }
+}
