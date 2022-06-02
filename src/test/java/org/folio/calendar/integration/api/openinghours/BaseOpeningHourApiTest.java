a13ea815c2d9030e8b025760d2e039f96e7ed275
Noah Overcash <ncovercash@ua.edu>
2022-06-02 01:57:18 -0400

    Add deletion unit tests (close #57)

--- src/test/java/org/folio/calendar/integration/api/openinghours/BaseOpeningHourApiTest.java
+++ src/test/java/org/folio/calendar/integration/api/openinghours/BaseOpeningHourApiTest.java
@@ -20,6 +20,7 @@ public abstract class BaseOpeningHourApiTest extends BaseApiAutoDatabaseTest {
 
   public static final String CREATE_CALENDAR_API_ROUTE = "/opening-hours/calendars";
   public static final String GET_CALENDAR_API_ROUTE = "/opening-hours/calendars/%s";
+  public static final String DELETE_CALENDAR_API_ROUTE = "/opening-hours/calendars/%s";
 
   @Autowired
   private CalendarMapper calendarMapper;
@@ -49,6 +50,25 @@ public abstract class BaseOpeningHourApiTest extends BaseApiAutoDatabaseTest {
         getRequestUrl(
           String.format(
             GET_CALENDAR_API_ROUTE,
+            ids.stream().map(UUID::toString).collect(Collectors.joining(","))
+          )
+        )
+      );
+  }
+
+  /**
+   * DELETE /opening-hours/calendars/{ids*} - Send a Calendar delete request
+   * @param ids the calendars to delete
+   * @return the Response
+   */
+  public Response sendCalendarDeleteRequest(List<UUID> ids) {
+    // spec must be ignored as the validator improperly flags comma-separated UUIDs
+    // as being illegal
+    return ra(ValidationSchema.NONE)
+      .delete(
+        getRequestUrl(
+          String.format(
+            DELETE_CALENDAR_API_ROUTE,
             ids.stream().map(UUID::toString).collect(Collectors.joining(","))
           )
         )
