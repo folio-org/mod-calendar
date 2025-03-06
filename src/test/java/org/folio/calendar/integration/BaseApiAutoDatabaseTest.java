598aae6958bb17ff77b70e005ed7222b91f89ea7
Noah Overcash <novercash@ebsco.com>
2025-03-06 10:49:47 -0500

    sonar nit

--- src/test/java/org/folio/calendar/integration/BaseApiAutoDatabaseTest.java
+++ src/test/java/org/folio/calendar/integration/BaseApiAutoDatabaseTest.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.TestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
@@ -19,7 +18,7 @@ public abstract class BaseApiAutoDatabaseTest extends BaseApiTest {
   protected boolean hasCreated = false;
 
   @AfterEach
-  void afterEach(TestInfo testInfo, @Autowired DataSource dataSource) {
+  void afterEach(@Autowired DataSource dataSource) {
     cleanDatabase(dataSource);
   }
 
